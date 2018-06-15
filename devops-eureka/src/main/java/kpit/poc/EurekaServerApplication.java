6b4967489a0f8202cf5094c3cf15edb11e26094d
Prabhash <kumarprabhash1995@gmail.com>
2018-06-15 15:29:23 +0530

    updating test branch

--- devops-eureka/src/main/java/kpit/poc/EurekaServerApplication.java
+++ devops-eureka/src/main/java/kpit/poc/EurekaServerApplication.java
@@ -3,7 +3,8 @@ package kpit.poc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-//Eureka Server Service
+//Eureka Server
+//test branch
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaServerApplication {
