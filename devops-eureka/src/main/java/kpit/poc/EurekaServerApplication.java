0787d788ff7850004841b977179c1accda7fba83
Kumar Prabhash Anand <kumarprabhash1995@gmail.com>
2018-06-15 13:35:57 +0530

    Update EurekaServerApplication.java

--- devops-eureka/src/main/java/kpit/poc/EurekaServerApplication.java
+++ devops-eureka/src/main/java/kpit/poc/EurekaServerApplication.java
@@ -3,7 +3,7 @@ package kpit.poc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-
+//Eureka Service
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaServerApplication {
